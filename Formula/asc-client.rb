494653b581b07f19fc4ce2309c13c42cee042c01
Kerem Erkan <kerem.erkan@gmail.com>
2026-02-24 22:45:12 +0300

    Update asc-client to 0.3.1

--- Formula/asc-client.rb
+++ Formula/asc-client.rb
@@ -1,13 +1,13 @@
 class AscClient < Formula
   desc "Command-line tool for the App Store Connect API"
   homepage "https://github.com/keremerkan/asc-client"
-  version "0.3.0"
+  version "0.3.1"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/keremerkan/asc-client/releases/download/v0.3.0/asc-client-macos-arm64.tar.gz"
-      sha256 "d03e1d9e4867db32e90238e8c4889c503efeb57bfc8309e5936f4b65ce8eb847"
+      url "https://github.com/keremerkan/asc-client/releases/download/v0.3.1/asc-client-macos-arm64.tar.gz"
+      sha256 "d66c4917bd756a0f6d0f1944f7f5bff4f39043c2c30744c0742df23b726fd085"
     end
   end
 
