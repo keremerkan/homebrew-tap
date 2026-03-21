4184579199122683313bb833d304cec0ad21da8c
Kerem Erkan <kerem.erkan@gmail.com>
2026-03-21 12:15:32 +0300

    ascelerate 0.9.1

--- Formula/ascelerate.rb
+++ Formula/ascelerate.rb
@@ -1,13 +1,13 @@
 class Ascelerate < Formula
   desc "A Swift CLI for App Store Connect"
   homepage "https://github.com/keremerkan/ascelerate"
-  version "0.9.0"
+  version "0.9.1"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/keremerkan/ascelerate/releases/download/v0.9.0/ascelerate-macos-arm64.tar.gz"
-      sha256 "ba4023e75d8b8b755785bea08955b3fb3d85555b2e880c0c61e5f76cb7e5943a"
+      url "https://github.com/keremerkan/ascelerate/releases/download/v0.9.1/ascelerate-macos-arm64.tar.gz"
+      sha256 "95ddcf74ed067ebfc58b1f2939e6b2ae2356a31d64e4bf71a9265924a4791994"
     end
   end
 
